f0e9bbccba768eeb52fae3b79dfb8f0c370b6b41
jmii1236 <148896255+jmii1236@users.noreply.github.com>
2024-12-03 21:35:48 -0500

    Add files via upload

--- IntervalTree.h
+++ IntervalTree.h
@@ -21,8 +21,8 @@ private:
                 : interval(interval), flightNumber(flightNumber), max(interval.second),
                   left(nullptr), right(nullptr), height(1) {}
     };
+    //creates interval tree root
     Node* root;
-
     //tracks height of different nodes
     int Height(Node *node) {
         if (node != nullptr) {
@@ -31,7 +31,7 @@ private:
             return 0;
         }
     }
-
+    //get the balance of a node for rebalancing purposes later
     int getBalance(Node *node) {
         if (node != nullptr) {
             return Height(node->left) - Height(node->right);
@@ -39,7 +39,7 @@ private:
             return 0;
         }
     }
-
+//taking from lectcure 4 balance trees
     Node *leftRotation(Node *node) {
         Node *newParent = node->right;
         Node *temp = newParent->left;
@@ -54,7 +54,6 @@ private:
 
         return newParent;
     }
-
 //taking from lectcure 4 balance trees
     Node *rightRotation(Node *node) {
         Node *newParent = node->left;
@@ -68,25 +67,22 @@ private:
 
         return newParent;
     }
-
 //insert nodes into the interval tree
     Node *insert(Node *root, pair<int, int> interval, const string &flightNumber) {
-        //creates root if not already exiisting
+        //creates root if not already existing
         if (root == nullptr) {
             return new Node(interval, flightNumber);
         }
         //get the low of the root node to be able to compare for proper sorting for future node insertions
         int lowVal = root->interval.first;
-
+        //sorting of node insertions
         if (interval.first < lowVal) {
             root->left = insert(root->left, interval, flightNumber);
         } else if (interval.first > lowVal) {
             root->right = insert(root->right, interval, flightNumber);
         }
-        //does this update correctly?? i think so but have to draw it out
+        //updates the max value
         root->max = max(root->max, interval.second);
-        //store the intervals so we check overlapping intervals later
-        //root->storageOfIntervals.push_back(interval);
 
         int treesBalance = getBalance(root);
 
@@ -113,86 +109,94 @@ private:
 
         return root;
     }
-
     //taking search from mod 4 lecture BST Search and Insert
-    void searchFlightNumber(Node* node, string flightNumber, vector<pair<int,int>> intervals) {
+    void searchFlightNumber(Node* node, const string &flightNumber, pair<int,int> interval) {
+        //if tree is empty there cannot be any findable flight number
+        interval.first = -1;
+        interval.second = -1;
         if(node == nullptr) {
             return;
         }
-        //goes through left side of tree
+        //store the interval of the found flight
         if(node->flightNumber == flightNumber) {
-            intervals.push_back(node->interval);
+            interval.first = node->interval.first;
+            interval.second = node->interval.second;
         }
-
-        searchFlightNumber(node->left, flightNumber, intervals);
-        searchFlightNumber(node->right, flightNumber, intervals);
+        //goes through left side of tree
+        searchFlightNumber(node->left, flightNumber, interval);
+        //goes through right side of tree
+        searchFlightNumber(node->right, flightNumber, interval);
     }
 //from geeksforgeeks interval tree implementation
     bool doOverlap(pair<int, int> interval1, pair<int, int> interval2) {
         return (interval1.first <= interval2.second && interval2.first <= interval1.second);
     }
-
 //from geeksforgeeks interval tree implementation
-    void overlapSearch(Node *root, pair<int, int> comparisonInterval, vector<Node *> &overlaps) {
-        if (root == nullptr) {
+    void overlapSearch(Node *node, pair<int, int> comparisonInterval, vector<Node *> &overlaps) {
+        //if tree is empty there cannot be any findable interval
+        if (node == nullptr) {
             return;
         }
-        if (doOverlap(root->interval, comparisonInterval)) {
-            overlaps.push_back(root);
+        //store the node of the found interval
+        if (doOverlap(node->interval, comparisonInterval)) {
+            overlaps.push_back(node);
         }
-
-        if (root->left != nullptr && root->left->max >= comparisonInterval.first) {
-            overlapSearch(root->left, comparisonInterval, overlaps);
+        //only searches left if the left node's max is greater than the comparisons first, saving time
+        if (node->left != nullptr && node->left->max >= comparisonInterval.first) {
+            overlapSearch(node->left, comparisonInterval, overlaps);
         }
+        //first of interval has to be less to be valid
         if (comparisonInterval.first > comparisonInterval.second) {
             cout << "Error: Invalid interval." << endl;
             return;
         }
-        overlapSearch(root->right, comparisonInterval, overlaps);
+        //searches in the right
+        overlapSearch(node->right, comparisonInterval, overlaps);
     }
-
 //inorder traversal if needed
-    void inorder(Node *root) {
-        if (root == nullptr) {
+    void inorder(Node *node) {
+        if (node == nullptr) {
             return;
         }
-        inorder(root->left);
-        cout << "[" << root->interval.first << ", " << root->interval.second << "]"
-             << " (Flight: " << root->flightNumber << ") max = " << root->max << endl;
-        inorder(root->right);
+        inorder(node->left);
+        cout << "[" << node->interval.first << ", " << node->interval.second << "]"
+             << " (Flight: " << node->flightNumber << ") max = " << node->max << endl;
+        inorder(node ->right);
     }
-
 //amount of bytes used calculator
-    size_t calculateSpace(Node *root) {
-        if (root == nullptr) {
+    size_t calculateSpace(Node *node) {
+        //size is nothing if tree is empty
+        if (node == nullptr) {
             return 0;
         } else {
-            return sizeof(*root) + calculateSpace(root->left) + calculateSpace(root->right);
+            //sizeof calculates the bytes of what it takes in
+            return sizeof(*node) + calculateSpace(node->left) + calculateSpace(node->right);
         }
     }
-
 //destructor to clear potential memory leaks
-    void destructor(Node *root) {
-        if (root == nullptr) {
+    void destructor(Node *node) {
+        //cant destroy if tree does not exist
+        if (node == nullptr) {
             return;
         }
-        destructor(root->left);
-        destructor(root->right);
-        delete root;
+        destructor(node->left);
+        destructor(node->right);
+        delete node;
     }
 public:
+    //contructor
     IntervalTree() {
         root = nullptr;
     }
-
+    //destructor
     ~IntervalTree() {
         destructor(root);
     }
-
+    //insert method
     void insert(pair<int, int> interval, const string& flightNumber) {
         root = insert(root, interval, flightNumber);
     }
-
+    //output a vector of all overlapping intervals
     vector<pair<int, int>> searchOverlaps(pair<int, int> comparisonInterval) {
         vector<Node *> overlaps;
         overlapSearch(root, comparisonInterval, overlaps);
@@ -203,22 +207,24 @@ public:
         }
         return results;
     }
-
-    vector<pair<int,int>> searchFlight(const string& flightNumber) {
-        vector<pair<int,int>> intervals;
-        searchFlightNumber(root, flightNumber, intervals);
-        if(intervals.empty()) {
+    //outputs a flight info if found
+    pair<int,int> searchFlight(const string& flightNumber) {
+        pair<int,int> interval;
+        searchFlightNumber(root, flightNumber, interval);
+        if(interval.first == -1 && interval.second == -1) {
             cout << "Flight " << flightNumber << " not found." << endl;
         }
         else {
             cout << "Flight found: " << endl;
             cout << "Interval [" << root->interval.first << ", " << root->interval.second << "] (Flight: " << root->flightNumber << ")" << endl;
         }
-        return intervals;
+        return interval;
     }
+    //gets the max value
     int getMax() {
         return root->max;
     }
+    //gets the flight number
     string getFlightNumber(const pair<int, int>& interval) {
         Node* node = root;
         while (node != nullptr) {
